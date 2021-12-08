84c1aad58c6722c033389294d64ca79b88a36721
xTheCoderx <justaguyoney@gmail.com>
2021-12-09 01:18:58 +1300

    The user can now draw on the canvas as they would in a standard pixel art editor canvas.

--- app/src/main/java/com/realtomjoney/canvas/MyCanvasView.kt
+++ app/src/main/java/com/realtomjoney/canvas/MyCanvasView.kt
@@ -1,16 +1,20 @@
 package com.realtomjoney.canvas
 
 import android.content.Context
-import android.graphics.Bitmap
-import android.graphics.Canvas
+import android.graphics.*
+import android.util.Log
+import android.view.MotionEvent
 import android.view.View
-import androidx.core.content.res.ResourcesCompat
+import android.widget.Toast
+import androidx.constraintlayout.widget.ConstraintSet
 
 class MyCanvasView(context: Context) : View(context) {
     private lateinit var extraCanvas: Canvas
     private lateinit var extraBitmap: Bitmap
 
-    private val backgroundColor = ResourcesCompat.getColor(resources, R.color.colorBackground, null)
+    private val spanCount = 10.0
+
+    private val rectangles = mutableListOf<RectF>()
 
     override fun onSizeChanged(w: Int, h: Int, oldw: Int, oldh: Int) {
         super.onSizeChanged(w, h, oldw, oldh)
@@ -20,7 +24,74 @@ class MyCanvasView(context: Context) : View(context) {
         extraBitmap = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888)
         extraCanvas = Canvas(extraBitmap)
 
-        extraCanvas.drawColor(backgroundColor)
+        val scale = (w / spanCount)
+
+        for (i in 0 until spanCount.toInt()) {
+            for (i_2 in 0 until spanCount.toInt()) {
+                val left = (i * scale).toFloat()
+                val top = (i_2 * scale).toFloat()
+                val rect = RectF(
+                    left,
+                    top,
+                    left + scale.toFloat(),
+                    top + scale.toFloat()
+                )
+                Log.d("MY_LOG", "LEFT: ${((i * scale).toFloat())} TOP: ${((i_2 * scale).toFloat())} ")
+
+                rectangles.add(rect)
+                extraCanvas.drawRect(
+                    rect,
+                    Paint().apply {
+                        style = Paint.Style.FILL
+                        color = Color.WHITE
+                    })
+            }
+        }
+
+        for (i in 0..spanCount.toInt()) {
+            for (i_2 in 0..spanCount.toInt()) {
+                extraCanvas.drawRect(
+                    (i * scale).toFloat(),
+                    (i_2 * scale).toFloat(),
+                    scale.toFloat(),
+                    scale.toFloat(),
+                    Paint().apply {
+                        color = Color.GRAY
+                        style = Paint.Style.STROKE
+                    })
+            }
+        }
+
+    }
+
+    fun validate(x: Float, y: Float) {
+        for (rect in rectangles) {
+            if (rect.contains(x, y)) {
+                extraCanvas.drawRect(
+                    rect,
+                    Paint().apply {
+                        style = Paint.Style.FILL
+                        color = Color.BLACK
+                    })
+                invalidate()
+            }
+        }
+    }
+
+    override fun dispatchTouchEvent(event: MotionEvent): Boolean {
+        val x = event.rawX
+        val y = event.rawY
+
+        when (event.actionMasked) {
+            MotionEvent.ACTION_MOVE -> {
+                validate(x, y)
+            }
+            MotionEvent.ACTION_DOWN -> {
+                validate(x, y)
+            }
+        }
+
+        return true
     }
 
     override fun onDraw(canvas: Canvas) {
