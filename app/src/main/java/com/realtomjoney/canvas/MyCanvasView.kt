70925ec3e0e15f74dd0d108d6f5407002ba4b323
xTheCoderx <justaguyoney@gmail.com>
2021-12-08 15:32:27 +1300

    Defined lateinit canvas and bitmap variables.

--- app/src/main/java/com/realtomjoney/canvas/MyCanvasView.kt
+++ app/src/main/java/com/realtomjoney/canvas/MyCanvasView.kt
@@ -1,7 +1,11 @@
 package com.realtomjoney.canvas
 
 import android.content.Context
+import android.graphics.Bitmap
+import android.graphics.Canvas
 import android.view.View
 
 class MyCanvasView(context: Context) : View(context) {
+    private lateinit var extraCanvas: Canvas
+    private lateinit var extraBitmap: Bitmap
 }
