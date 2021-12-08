7783218364c670513214678b40641af01b665759
xTheCoderx <justaguyoney@gmail.com>
2021-12-08 15:35:04 +1300

    Added class level variable which represents the background color of the canvas. It is initialized by default to the `colorBackground` defined earlier.

--- app/src/main/java/com/realtomjoney/canvas/MyCanvasView.kt
+++ app/src/main/java/com/realtomjoney/canvas/MyCanvasView.kt
@@ -4,8 +4,11 @@ import android.content.Context
 import android.graphics.Bitmap
 import android.graphics.Canvas
 import android.view.View
+import androidx.core.content.res.ResourcesCompat
 
 class MyCanvasView(context: Context) : View(context) {
     private lateinit var extraCanvas: Canvas
     private lateinit var extraBitmap: Bitmap
+
+    private val backgroundColor = ResourcesCompat.getColor(resources, R.color.colorBackground, null)
 }
